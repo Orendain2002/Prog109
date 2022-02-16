db2459096c261102408f9e24f84ab0f3db7fdc2d
Orendain2002 <97069476+Orendain2002@users.noreply.github.com>
2022-02-16 03:42:48 -0800

    Update validator.js

--- validation/validator.js
+++ validation/validator.js
@@ -4,7 +4,8 @@ var validLastname=false;
 var validEmail=false;
 var validPhone=false;
 var validUsername=false;
-var validPasswrod=false;
+var validPassword=false;
+var validCountry=false;
 var validZipcode=false;
 
 var firstname = document.getElementById("FirstName").value;
@@ -15,43 +16,58 @@ var dotpos = userEmail.lastIndexOf(".");
 var phone = document.getElementById("Phone").value;
 var username = document.getElementById("Username").value;
 var password = document.getElementById("Password").value;
+var country = document.getElementById("Country").value;
 var zipcode = document.getElementById("Zipcode").value;
 
 if (firstname==="null" || firstname==="" || firstname.length > 20) {
     errorMessages += "<p>The firstname is required and cannot be greater than 20 characters</p>";
 }
+else {
+   validFirstname = true;
+}
 if (lastname==="null" || lastname==="" || lastname.length > 20) {
     errorMessages += "<p>The lastname is required and cannot be greater than 20 characters</p>";
+}
+else {
+   validLastname = true;
 }
 if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=userEmail.length) {
     errorMessages += "<p>Invalid email</p>";
 }
-if (isNaN(phone) || phone.lenght >15 || phone===null || phone==="") {
+else {
+   validEmail = true;
+}
+if (isNaN(phone) || phone.length >15 || phone===null || phone==="") {
    errorMessages += "<p>Invalid phone number </p>";
+}
+else {
+   validPhone = true;
 }
 if (username==="null" || username==="" || username.length > 12) {
     errorMessages += "<p>The username is required and cannot be greater than 12 characters</p>";
 }
+else{
+   validUsername = true;
+}
 if (password==="null" || password==="" || password.length > 7) {
     errorMessages += "<p>The password is required and cannot be greater than 7 characters</p>";
 }
-else {
-   validFirstname = true;
-   validLastname = true;
-   validEmail = true;
-   validPhone = true;
-   validUsername = true;
+else{
    validPassword = true;
+}
+if (country === "USA") {
+   errorMessages += "<p>Zipcode is required</p>"
+}
+else{
+   validCountry = true;
+}
+if (zipcode.length > 5) {
+   errorMessages += "<p>Zipcode cannot be greater than 5 digits.</p>"
+}
+else {
    validZipcode = true;
 }
-
 document.getElementById("errorMessages").innerHTML = errorMessages;
 
-return (validFirstname);
-return (validLastname);
-return (validEmail);
-return (validPhone);
-return (validUsername);
-return (validPassword);
-return (validZipcode);
+return (validFirstname && validLastname && validEmail && validPhone && validUsername && validPassword && validZipcode);
 }
